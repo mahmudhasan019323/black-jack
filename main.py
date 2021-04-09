fcb2a7fd6c13860c6343ab626ba6f744ffc84cda
Mahmudul Hasan <mahmudhasan019323@gmail.com>
2021-04-09 23:39:45 +0600

    optimize the game

--- main.py
+++ main.py
@@ -5,50 +5,40 @@ cards = [11,2,3,4,5,6,7,8,9,10,10,10]
 def get_card(arr):
     arr.append(random.choice(cards))
 
+def show_result(player_list,dealer_list,msg):
+    print(f"player final hand  {player_list}: {sum(player_list)}")
+    print(f"dealer final hand  {dealer_list}: {sum(dealer_list)}")
+    print(msg)
+    continue_game = input("wanna play a game of blackjack. type 'y' or 'n': ").lower()
+    if continue_game == 'y':
+        black_jack()
+    else:
+        exit()
+
 def black_jack():
     print('\033c')
     print(logo)
     game_over = False
     player = random.choices(cards, k=2)
-    # player = [10,4]
     dealer = random.choices(cards, k=2)
     while not game_over:
         print(f"your cards: {player}, current score: {sum(player)}")
         print(f"dealer's first card: {dealer[0]}")
         if sum(player) == 21:
-            print(f"player final hand  {player}: {sum(player)}")
-            print(f"dealer final hand  {dealer}: {sum(dealer)}")
-            print("you win with blackjack")
-            continue_game = input("wanna play a game of blackjack. type 'y' or 'n': ").lower()
-            if continue_game == 'y':
-                black_jack()
-            else:
-                exit()
+            show_result(player,dealer,'you won with blackjack')
+            
         elif sum(player) > 21:
             if player.count(11) != 0 and ((sum(player)-10) <= 21):
                 if (sum(player) - 10) == 21:
-                    print(f"player final hand  {player}: {sum(player) -10}")
-                    print(f"dealer final hand  {dealer}: {sum(dealer)}")
-                    print("you win with blackjack")
-                    continue_game = input("wanna play a game of blackjack. type 'y' or 'n': ").lower()
-                    if continue_game == 'y':
-                        black_jack()
-                    else:
-                        exit()
+                    show_result(player,dealer, 'you won with blackjack')
+                    
                 else:
                     player[player.index(11)] = 1
             else:
-                print(f"player final hand  {player}: {sum(player)}")
-                print(f"dealer final hand  {dealer}: {sum(dealer)}")
-                print("you lose with bust")
-                continue_game = input("wanna play a game of blackjack. type 'y' or 'n': ").lower()
-                if continue_game == 'y':
-                    black_jack()
-                else:
-                    exit()
+                show_result(player,dealer, 'you lost with bust')
+                
         else:
             hit = input("get another card? type 'y' or 'n': ").lower()
-            # hit = 'n'
             if hit == 'y':
                 get_card(player)
             else:
@@ -58,51 +48,21 @@ def black_jack():
                 if sum(dealer) > 21:
                     if dealer.count(11) != 0 and ((sum(dealer)-10) <= 21):
                         if (sum(dealer) - 10) == 21:
-                            print(f"player final hand  {player}: {sum(player) -10}")
-                            print(f"dealer final hand  {dealer}: {sum(dealer)}")
-                            print("you win with blackjack")
-                            continue_game = input("wanna play a game of blackjack. type 'y' or 'n': ").lower()
-                            if continue_game == 'y':
-                                black_jack()
-                            else:
-                                exit()
+                            show_result(player,dealer, 'you won with blackjack')
+                            
                     else:
-                        print(f"player final hand  {player}: {sum(player)}")
-                        print(f"dealer final hand  {dealer}: {sum(dealer)}")
-                        print("you lose with bust")
-                        continue_game = input("wanna play a game of blackjack. type 'y' or 'n': ").lower()
-                        if continue_game == 'y':
-                            black_jack()
-                        else:
-                            exit()
+                        show_result(player,dealer, 'you lost with bust')
+                        
                 else:
                     if sum(player) > sum(dealer):
-                        print(f"player final hand  {player}: {sum(player)}")
-                        print(f"dealer final hand  {dealer}: {sum(dealer)}")
-                        print("you WIN!")
-                        continue_game = input("wanna play a game of blackjack. type 'y' or 'n': ").lower()
-                        if continue_game == 'y':
-                            black_jack()
-                        else:
-                            exit()
+                        show_result(player,dealer, 'you win')
+                        
                     elif sum(player) < sum(dealer):
-                        print(f"player final hand  {player}: {sum(player)}")
-                        print(f"dealer final hand  {dealer}: {sum(dealer)}")
-                        print("you lose!")
-                        continue_game = input("wanna play a game of blackjack. type 'y' or 'n': ").lower()
-                        if continue_game == 'y':
-                            black_jack()
-                        else:
-                            exit()
+                        show_result(player,dealer, 'you lose')
+                        
                     else:
-                        print(f"player final hand  {player}: {sum(player)}")
-                        print(f"dealer final hand  {dealer}: {sum(dealer)}")
-                        print("it's a draw")
-                        continue_game = input("wanna play a game of blackjack. type 'y' or 'n': ").lower()
-                        if continue_game == 'y':
-                            black_jack()
-                        else:
-                            exit()
+                        show_result(player,dealer, "it's a draw" )
+                        
 black_jack()
 
 
